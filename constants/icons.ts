89da5b485e0749809b34f44e32e9fca280133ecd
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2025-02-26 21:23:42 +0000

    icons

---
+++ constants/icons.ts
@@ -0,0 +1,44 @@
+
+
+
+import bookmark from '../assets/icons/bookmark.png';
+import home from '../assets/icons/home.png';
+import plus from '../assets/icons/plus.png';
+import profile1 from '../assets/icons/profile1.png';
+import leftArrow from '../assets/icons/left-arrow.png';
+import menu from '../assets/icons/menu.png';
+import search from "../assets/icons/search-fill.svg";
+import upload from '../assets/icons/upload.png';
+import logout from '../assets/icons/logout.png';
+import eyeHide from '../assets/icons/eye-hide.png';
+import eye from '../assets/icons/eye.png';
+
+export default {
+  bookmark,
+  home,
+  plus,
+  profile1,
+  leftArrow,
+  menu,
+  search,
+  upload,
+  logout,
+  eyeHide,
+  eye,
+}
+
+// export default {
+//   bookmark,
+//   home,
+//   plus,
+//   // profile,
+//   leftArrow,
+//   menu,
+//   // search,
+//   upload,
+//   logout,
+//   eyeHide,
+//   eye,
+// };
+
+
